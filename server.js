450a059c43b803b158a9f8ffa643fd5751b6d158
Vaden89 <isaacshosanya89@gmail.com>
2025-02-03 18:43:11 +0100

    changed response format to meet required format

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ app.get("/info", (req, res) => {
   const date = new Date();
   res.status(200).json({
     message: "Success",
-    data: {
-      email: "isaacshosanya89@gmail.com",
-      current_datetime: date.toISOString(),
-      github_url: "https://github.com/Vaden89/HNGi2-Backend-Task-0.git",
-    },
+    email: "isaacshosanya89@gmail.com",
+    current_datetime: date.toISOString(),
+    github_url: "https://github.com/Vaden89/HNGi2-Backend-Task-0.git",
   });
 });
 
